50f3c51fb0a088fbcacc3a22c14f15e5b96ed638
saitovaan35 <saitovaan35@gmail.com>
2025-01-31 10:06:17 +0000

    functionschecked

---
+++ assets/index.js
@@ -0,0 +1,13 @@
+function сказатьПривет (){
+    console.log (`Good afternoon, dear ladies`);
+}
+сказатьПривет ();
+
+let FunctionExpression = function () {
+    console,log ("Second way");
+};
+
+let ArrowFunction = () => console.log ("third way");
+
+сказатьПривет ();
+FunctionExpression;
